af2f364162357bdf39343dbb8aef45136ac9863b
Brad Kendall <bkendall@sonicwall.com>
2019-05-28 17:56:48 +0000

    WWW2-1308,

---
+++ wp-content/uploads/custom-css-js/52087.js
@@ -0,0 +1,14 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+jQuery(function(){
+        jQuery('#link-to-privacy-policy').bind("mouseenter",function(){
+            jQuery('#privacy-policy').stop().show();
+        });
+        jQuery('#link-to-privacy-policy').bind("mouseleave",function(){
+            jQuery('#privacy-policy').stop().hide();
+        })
+    });
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
