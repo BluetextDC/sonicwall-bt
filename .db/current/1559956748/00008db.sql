ae99e86526511751bd6991a856fc87acc66cc711
Brad Kendall <bkendall@sonicwall.com>
2019-06-08 02:27:29 +0000

    fixed WPML string issue

--- .db/current/1559956748/00008db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5d9d13597007d1ef79ff30e6745f15a89aea38e9722e621f342c3ac132822e97
-size 49711653
--- .db/current/1559956748/00020db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96d537858b8460bf197df8b0b8742df29f26e0f5ab6797035b1449dae81613f7
-size 49621966
