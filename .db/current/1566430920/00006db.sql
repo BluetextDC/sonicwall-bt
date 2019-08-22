3e46815a3a5fa0e914110b82398edf4c04fd6013
Brad Kendall <bkendall@sonicwall.com>
2019-08-22 00:12:05 +0000

    Translation and redirect updates

--- .db/current/1566430920/00006db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:306479c0f614b79eb1ac1e9dbecec56c0dd515a828acf9eba89915bd7d4815e3
-size 49973382
--- .db/current/1566430920/00023db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e5f24cdebea98c81007ef7df4b608e7bcbb9c765a63239dc14e5d874aac94628
-size 48975775
