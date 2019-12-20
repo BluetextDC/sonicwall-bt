fad8765e647a33393f5e1627c7c3c582c59a7f96
Brad Kendall <bkendall@sonicwall.com>
2019-12-20 01:22:16 +0000

    December 19, 2019

---
+++ .db/current/1576804869/00002db.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c2aa0b10b732e411a9f99571186ece6fe3f2d7a6ecfa9100c71d11c02b60aa6
+size 49338120
--- wp-content/plugins/right-answers/templates/ra-single-alert.php
+++ wp-content/plugins/right-answers/templates/ra-single-alert.php
@@ -1,30 +1,54 @@
 <?php
 
+// $request_uri = strpos( $_SERVER['REQUEST_URI'], '/product-notification' );
 
-	if ( isset( $_REQUEST['sol_id'] ) ){
+if ( isset( $_REQUEST['sol_id'] ) ) { //  &&  ($request_uri !== false)
 
-        $key = $_REQUEST['sol_id'] . '_solution';
-        
-		if ( false === ( $main_data = getRACacheItem($key) ) ) {
-		  // It wasn't there, so regenerate the data and save the transient
-		  $main_data = single_solution_search( $_REQUEST['sol_id'], true );
-            
-          setRACacheItem($key, $main_data);
-		}
+    global $wp_query;
+
+    if (isset($wp_query->query_vars['kb-slug'])) {
+        $main_data = single_solution_search( $_REQUEST['sol_id'], true);  
         
         //If the RA type is "Solutions", redirect to the same slug but with knowledge-base instead of product-notification
-        if ($main_data->type == "Solutions")
-        {
+        if ($main_data->type == "Solutions") {
             global $wp;
             $url = home_url( $wp->request );
             $url = str_replace("/support/product-notification/","/support/knowledge-base/",$url);
-            if (wp_redirect($url, 301))
-            {
+            if (wp_redirect($url, 301)) {
                 exit();
             }
         }
+    } else {
+        global $wpdb;
 
-	}
+        $sql = "SELECT * FROM {$wpdb->prefix}ra_slugs WHERE sol_id=%s LIMIT 1";
+        $sql = $wpdb->prepare($sql, $_REQUEST['sol_id']);
+
+        $result = $wpdb->get_results($sql);
+        
+        // $test_sql = "SELECT * FROM {$wpdb->prefix}ra_slugs d WHERE d.sol_id=%s ORDER BY created_at DESC";
+        // $test_sql = $wpdb->prepare($test_sql, $_REQUEST['sol_id']);
+        // $test_res= $wpdb->get_results($test_sql);
+        // echo '<pre>'.json_encode($sql).json_encode($result).'</pre>';
+        // echo '<pre>'.json_encode($test_sql).json_encode($test_res).'</pre>';
+        // die();
+        if ($result && count($result) > 0) {
+            //Redirect to the proper url
+            if ( wp_redirect( "/support/product-notification/".$result[0]->slug."/".$_REQUEST['sol_id']."/", 301) ) {
+                exit;
+            }
+        } else {
+            //Failed to find slug, set 404
+            ra_404();
+        }
+    }
+} else 
+// if ( $request_uri !== false ) {
+    //Redirect to support home
+    if ( wp_redirect( "/support" ) ) {
+        exit;
+    }
+// }
 
 ?>
 
